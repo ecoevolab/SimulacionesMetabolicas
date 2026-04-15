4e4a2fbe2b88627675c6d1d636d2a83512ff0cd4
myabiabi <abigail.montante.arenas@gmail.com>
2026-04-15 10:08:32 -0600

    deleted:    2results.error 	deleted:    2results.log 	deleted:    3results.error 	deleted:    3results.log 	deleted:    4c_biomasas_prokka_carveme_lb_lb.py 	deleted:    4c_comparacion_curvas_prokka_carveme_lb_lb.py 	deleted:    4c_curvas_individuales_prokka_carveme_lb_lb.py 	deleted:    4results.error 	deleted:    4results.log 	deleted:    barplot.R 	deleted:    barrplot.R 	deleted:    comets_function_2.py 	deleted:    comets_save_2.py     deleted:    comets_save_3.py 	deleted:    comets_save_4.py 	deleted:    comets_save_indi.py 	deleted:    ecoli_core_2.py 	deleted:    indi_results.error 	deleted:    indi_results.log 	deleted:    procesando_resultados.py 	deleted:    process_results.py 	deleted:    resultados.R 	deleted:    resultados.py 	deleted:    resultados_2.py 	deleted:    results.error 	deleted:    results.log 	deleted:    results.py 	deleted:    results.slurm 	deleted:    results_2.slurm 	deleted:    results_3.slurm 	deleted:    results_4.slurm 	deleted:    results_indi.slurm 	deleted:    temp/COMETS_manifest.txt 	deleted:    temp/ST00060_prokka_carveme_lb.cmd 	deleted:    temp/ST00094_prokka_carveme_lb.cmd 	deleted:    temp/ST00110_prokka_carveme_lb.cmd 	deleted:    temp/ST00143_prokka_carveme_lb.cmd 	deleted:    temp/ST00164_prokka_carveme_lb.cmd

--- procesando_resultados.py
+++
@@ -1,5 +0,0 @@
-from process_results import process_comets
-
-process_comets(folder_pkl='/mnt/data/sur/users/mmontante/04_resultados/rizo/resultados',
- output_folder='/mnt/data/sur/users/mmontante/04_resultados/rizo/resultados_procesados',
- time_step=0.1)
