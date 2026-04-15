46173e704e65d840655fced1800df6534730da70
myabiabi <abigail.montante.arenas@gmail.com>
2026-04-14 19:02:56 -0600

    modified:   sim_syncom_comets.py

--- sim_syncom_comets.py
+++ sim_syncom_comets.py
@@ -51,8 +51,8 @@ if __name__ == "__main__":
     # Prepare simulation
     # print(sim_params.get_param("TotalBiomassLogName"))
     # print(sim_params.get_param("MediaLogName"))
-    os.mkdir('temp', exist_ok=True)  # Create a temporary directory for COMETS output   
-    sim = c.comets(layout = layout, parameters = sim_params, relative_dir = 'temp/')
+    os.makedirs('/mnt/data/sur/users/mmontante/outputdir/temp', exist_ok=True)
+    sim = c.comets(layout = layout, parameters = sim_params, relative_dir = '/mnt/data/sur/users/mmontante/outputdir/temp')
     # Very ugly, but I need to redefine the output filenames
     # https://github.com/segrelab/cometspy/issues/64
     sim.parameters.set_param("BiomassLogName", os.path.join(args["outdir"], "biomass.txt"))
